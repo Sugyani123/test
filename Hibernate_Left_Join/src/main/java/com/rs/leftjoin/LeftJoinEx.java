db557f1ef32ce8f7ff7396058c2cea288d04e3f6
MY PC <MY PC@MYPC-PC>
2019-12-02 16:11:57 +0530

    Hi please start working this

---
+++ Hibernate_Left_Join/src/main/java/com/rs/leftjoin/LeftJoinEx.java
@@ -0,0 +1,33 @@
+package com.rs.leftjoin;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class LeftJoinEx {
+	public static void main(String args[])
+	{
+
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml"); 
+
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();		
+
+		Query qry= session.createQuery("select v.vendorName, c.customerName from Vendor v Left Join v.children c");
+
+		List l = qry.list();
+		Iterator it=l.iterator();
+
+		while(it.hasNext())
+		{
+			Object rows[] = (Object[])it.next();
+			System.out.println(rows[0]+ " -- " +rows[1]);
+		}
+
+	}
+
+}
